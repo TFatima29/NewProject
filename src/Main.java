271fb45ef20d8aeb83a9f8fe1ad99d69fff4e9de
TFatima29 <fatima33fatima333@gmail.com>
2026-02-12 19:00:50 +0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     static void main() {
         System.out.println("Новый проект в гите");
+
+
+        System.out.println("sdkjfksjdfnj;sdf");
+        System.out.println("dfklsdmf'lds");
+        int a;
     }
 }
